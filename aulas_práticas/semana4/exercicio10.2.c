f63c51e015d8d177922f1638cbd9f4f3581473de
cassio <antoniocassio000@gmail.com>
2026-09-05 23:29:14 -0300

    add: implement program to print a pyramid pattern based on user-defined height

--- aulas_práticas/semana4/exercicio10.2.c
+++ aulas_práticas/semana4/exercicio10.2.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    
+    int height, test = 0;
+    printf("enter the value of the height: ");
+    scanf("%d", &height);
+
+    for(int i = 1; i <= height; i++){
+        for(int k = height; k >= i; k--){
+            printf(" ");
+        }
+
+        for(int j = (2 * i) - 1; j >= 1; j--){
+            printf("*");}
+        
+    printf("\n");}
 }
