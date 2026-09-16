c60f4b888537c49b4eb54b4266a4ecc1c6e67f17
cassio <antoniocassio000@gmail.com>
2026-09-16 19:11:24 -0300

    add: implement program to generate and display a random matrix and its transpose

---
+++ aulas_teoricas/exercicio10_transposta.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define L 10
+#define C 10
+
+int main(){
+    srand(time(NULL));
+    int min = 1;
+    int max = 99;
+    int matriz[L][C];
+    int matriz_transposta[L][C];
+
+    printf("MATRIZ NORMAL:\n");
+
+    for(int i = 0; i < L; i++){
+        for(int j = 0; j < C; j++){
+            matriz[i][j] = (min + (rand() % (max - min + 1)));
+            printf("%d ", matriz[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\nMATRIZ TRANSPOSTA:\n");
+    
+    for(int j = 0; j < L; j++){
+        for(int i = 0; i < C; i++){
+            matriz_transposta[j][i] = matriz[i][j];
+            printf("%d ", matriz_transposta[j][i]);
+        }
+
+        printf("\n");
+    }
+
+    
+}
