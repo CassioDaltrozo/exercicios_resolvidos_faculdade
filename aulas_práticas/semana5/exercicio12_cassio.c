a35c04f0e871e1db17a0be945673b1fa2035f585
cassio <antoniocassio000@gmail.com>
2026-09-21 20:24:46 -0300

    fix: correct frequency calculation and improve output formatting

--- aulas_práticas/semana5/exercicio12_cassio.c
+++ aulas_práticas/semana5/exercicio12_cassio.c
@@ -9,35 +9,28 @@ int main(){
 
     int conjunto[TAMANHO];
     int max = 10, min = 1;
-    int frequencia = 0;
+    int frequencia[11] = {0};
     float media = 0.0;
 
-    //GERANDO O VETOR, VERIFICANDO A MEDIA E IMPRIMINDO
+    //GERANDO O VETOR, FAZENDO MÉDIA, CONJUGANDO FREQUENCIA
     printf("VETOR GERADO: \n");
     for(int i = 0; i < TAMANHO; i++){
         conjunto[i] = (min + (rand() % (max - min + 1)));
         media += conjunto[i];
         printf("%d ", conjunto[i]);
+        frequencia[conjunto[i]]++;
     }
     media /= TAMANHO;
 
-
-    //LOOP PARA FREQUENCIA USANDO CONTADOR
-    printf("\n\nFREQUENCIA ABSOLUTA: \n");
-
-    for(int i = 1; i <= max; i++){
-        frequencia = 0;
-        for(int j = 0; j <= TAMANHO; j++){
-            if(conjunto[j] == i){
-                frequencia++;
-            }
-        }
-        printf("NUMERO: %d apareceu %d vezes\n", i, frequencia);
+    //IMPRIMINDO FREQUENCIA 
+    printf("\n\n");
+    for(int i = 1; i < 11; i++){
+        printf("NUMERO %d APARECE %d vezes\n", i, frequencia[i]);
     }
 
     //TESTANDO CONDIÇÃO DA MEDIA
     printf("\nMEDIA: %.2f\n", media);
-
+    
     printf("NUMEROS MAIORES QUE A MEDIA: ");
     for(int i = 0; i < TAMANHO; i++){
         if(conjunto[i] > media){
